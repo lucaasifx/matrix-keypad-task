bf96d85f31c00fe404b16f6ec32b4485b294ecc4
GuilhermeLopes <guife.gf@hotmail.com>
2025-01-17 15:09:28 -0300

    função pra acionar o led verde se pressionado o botao "1"
    fiz algumas funçoes basicas para o funcionamento dos leds, e fiz a função que faz o led verde ligar caso o botao "1" do keypad seja pressionado

--- src/lib/led.c
+++ src/lib/led.c
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include "led.h"
 
+<<<<<<< Updated upstream
 // *********************** Lib padrao ***********************
 
 void init_led(uint8_t LED) {
@@ -29,3 +30,29 @@ void led_action(char key) {
 }
 
 
+=======
+// função de exemplo
+void initialize_led(uint8_t led) {
+    gpio_init(led);
+    gpio_set_dir(led, GPIO_OUT);
+}
+
+void led_on(uint8_t led){
+    gpio_put(led,true);
+}
+
+void led_off(uint8_t led){
+    gpio_put(led,false);
+}
+
+void led_action(char key){
+    if (key=='1')
+    {
+        led_on(LED_GREEN);
+    }else
+    {
+        led_off(LED_GREEN);
+    }
+}
+
+>>>>>>> Stashed changes
--- src/lib/led.h
+++ src/lib/led.h
@@ -6,6 +6,7 @@
 #define LED_BLUE 12
 #define LED_RED 13
 
+<<<<<<< Updated upstream
 // funções padrão
 void init_led(uint8_t);
 void turn_led_on(uint8_t);
@@ -13,6 +14,12 @@ void turn_led_off(uint8_t);
 void blink_led(uint8_t, uint32_t);
 
 // tarefa
+=======
+// função de exemplo
+void initialize_led(uint8_t);
+void led_on(uint8_t);
+void led_off(uint8_t);
+>>>>>>> Stashed changes
 void led_action(char);
 
 #endif
