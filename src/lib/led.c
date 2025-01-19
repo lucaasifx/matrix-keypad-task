5eb591dfadae0176643f809badb196170caf57b1
Lucas Emanoel <128146913+lucaasifx@users.noreply.github.com>
2025-01-19 00:07:41 -0300

    Implementação do Blink do LED verde com acionamento do Keypad

--- src/lib/led.c
+++ src/lib/led.c
@@ -16,35 +16,25 @@ void turn_led_off(uint8_t LED) {
     gpio_put(LED, false);
 }
 
-void blink_led(uint8_t LED, uint32_t ms) {
-    turn_led_on(LED);
-    sleep_ms(ms);
-    turn_led_off(LED);
-    sleep_ms(ms);
+void blink_led(uint8_t LED, uint32_t ms, uint8_t cycles) {
+    for(int i = 0; i < cycles; i++) {
+        turn_led_on(LED);
+        sleep_ms(ms);
+        turn_led_off(LED);
+    }
 }
 
 // **********************************************************
 
 void led_action(char key) {
-    static bool is_blinking = false;
-
-    if (key == '4') {
-        if (is_blinking) {
-            is_blinking = false;
-            turn_led_off(LED_GREEN);
-        } else {
-            is_blinking = true;
-            while (is_blinking) {
-                blink_led(LED_GREEN, 500);
-                sleep_ms(500);
-
-                char pressed_key = get_key();
-                if (pressed_key == '4') {
-                    is_blinking = false;
-                    turn_led_off(LED_GREEN);
-                    break;
-                }
-            }
+    if (key=='3'){
+        if (gpio_get(LED_RED)){
+            turn_led_off(LED_RED);
+        }else{
+            turn_led_on(LED_RED);
         }
+    }
+    if (key == '4') {
+        blink_led(LED_GREEN, 1000);
     }
 }
