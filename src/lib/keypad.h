7b4789ee788cb328164ee961bda3be333704dc60
Lucas Emanoel <lucaasifx@gmail.com>
2025-01-18 21:18:32 -0300

    Implementação do Keypad

--- src/lib/keypad.h
+++ src/lib/keypad.h
@@ -1,6 +1,19 @@
 #ifndef KEYPAD_H
 #define KEYPAD_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BUTTON_COUNT 16
+
+typedef struct {
+    uint8_t buttons[BUTTON_COUNT];
+    uint8_t gpio_column[4];
+    uint8_t gpio_rows[4];
+} Keypad;
+
+void keypad_init(Keypad *keypad);
+char keypad_get_key(Keypad *keypad);
 
 
 
